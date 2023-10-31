4e5705da9444c32880cf8fec1f5e80e55de353a3
sarvesh-mayil <sarvesh.mayil@tamu.edu>
2023-10-31 17:22:15 -0400

    added leaderboard, scenario runner paths

--- set_paths.sh
+++ set_paths.sh
@@ -6,3 +6,5 @@ export CARLA_ROOT=${SCRIPT_DIR}/carla
 export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI
 export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI/carla
 export PYTHONPATH=$PYTHONPATH:$CARLA_ROOT/PythonAPI/carla/dist/carla-0.9.14-py3.7-linux-x86_64.egg
+export PYTHONPATH=$PYTHONPATH:${SCRIPT_DIR}/leaderboard
+export PYTHONPATH=$PYTHONPATH:${SCRIPT_DIR}/scenario_runner
