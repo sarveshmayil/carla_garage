ff524b5c2da8ec46d5a1257b96110e77dc701bd6
aartran <aartran@umich.edu>
2023-11-28 17:31:33 -0500

    Added traffic manager, currently spawns 30 cars somewhere around route

--- project/run_controller.py
+++ project/run_controller.py
@@ -5,12 +5,13 @@ from autonomous_agent import Agent
 
 from agents.navigation.global_route_planner import GlobalRoutePlanner
 
+from traffic import TrafficUwU
 
 if __name__ == "__main__":
     client = carla.Client("localhost", 2000)
     client.set_timeout(10.0)
     world:carla.World = client.load_world('Town01')
-
+    
     settings = world.get_settings()
     settings.no_rendering_mode = True
     settings.synchronous_mode = True
@@ -22,6 +23,8 @@ if __name__ == "__main__":
     a = carla.Location(spawn_points[3].location)
     b = carla.Location(spawn_points[100].location)
 
+    traffic_man = TrafficUwU(idxA = 3, idxB = 100)
+    
     # vehicle = Vehicle(world=world)
     vehicle = Agent(world=world)
     vehicle.spawn(location=carla.Location(spawn_points[3].location))
@@ -29,4 +32,5 @@ if __name__ == "__main__":
     vehicle.planner = GlobalRoutePlanner(wmap, sampling_resolution=10)
     vehicle.set_route(start=a, target=b)
     # vehicle.follow_route(target_speed=30, visualize=True, debug=True)  # For Vehicle
-    vehicle.follow_route(visualize=True, debug=True)  # For Agent
+    traffic_man.spawn_traffic(numCars= 30)
+    vehicle.follow_route(visualize=True, debug=False)  # For Agent
